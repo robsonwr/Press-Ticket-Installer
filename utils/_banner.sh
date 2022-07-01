ce36ef110ee0d4989478b0f598343d4f3e772899
Robson Rissato <robsonwr@imagenet.com.br>
2022-07-01 13:12:09 -0300

    Arquivos para Instalação

---
+++ utils/_banner.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Print banner art.
+#######################################
+# Print a board. 
+# Globals:
+#   BG_BROWN
+#   NC
+#   WHITE
+#   CYAN_LIGHT
+#   RED
+#   BLUE
+#   GREEN
+#   YELLOW
+# Arguments:
+#   None
+#######################################
+print_banner() {
+
+  clear
+
+  printf "\n\n"
+
+  printf "${CYAN_LIGHT}";
+  printf "              .-"^`\                                        /`^"-.             \n";
+  printf "            .'   ___\                                      /___   `.             \n";
+  printf "           /    /.---.                                    .---.\    \             \n";
+  printf "          |    //     '-.  ___________________________ .-'     \\    |             \n";
+  printf "          |   ;|         \/--------------------------//         |;   |             \n";
+  printf "          \   ||       |\_)        Instalador        (_/|       ||   /             \n";
+  printf "           \  | \  . \ ;  |       Press-Ticket       || ; / .  / |  /             \n";
+  printf "            '\_\ \\ \ \ \ |                          ||/ / / // /_/'             \n";
+  printf "                  \\ \ \ \|      Linux Debian 11     |/ / / //             \n";
+  printf "                   `'-\_\_\       Robson Risato      /_/_/-'`             \n";
+  printf "                          '--------------------------'             \n";
+  printf "\n" 
+   
+  printf "${NC}";
+
+  printf "\n"
+}
